263933ea9555da677ac18358483d6da6543a61dd
Adam Neilson <adam@junkey.com>
2013-11-03 23:09:06 +0000

    added complexified and fiendishly hard outputs

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
-                 [ring "0.3.8"]
-                 [ring/ring-core "1.1.8"]
-                 [ring/ring-json "0.1.2"]
-                 [ring/ring-jetty-adapter "1.1.8"]
+                 [compojure "1.1.6"]
+                 [ring "1.2.1"]
+                 [ring/ring-core "1.2.1"]
+                 [ring/ring-json "0.2.0"]
+                 [ring/ring-jetty-adapter "1.2.1"]
                  [org.clojure/data.csv "0.1.2"]
-                 [com.taoensso/timbre "1.5.2"]
+                 [com.taoensso/timbre "2.6.3"]
                  [de.ubercode.clostache/clostache "1.3.1"]]
-  :plugins [[lein-ring "0.8.5"]]
+  :plugins [[lein-ring "0.8.8"]]
   :ring {:handler passw.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
--- src/passw/handler.clj
+++ src/passw/handler.clj
@@ -26,6 +26,64 @@
                   "-" "+" "=" "_" "." "/"
                   ))
 
+(def replacements {
+                   :a ["a" "A" "@"]
+                   :b ["b" "B" "ß" "8" "6"]
+                   :c ["c" "C"  ] 
+                   :d ["d" "D"]
+                   :e ["e" "E" "£" "3"]
+                   :f ["f" "F"]
+                   :g ["g" "G" ]
+                   :h ["h" "H" ]
+                   :i ["i" "I" "1"]
+                   :j ["j" "J" ]
+                   :k ["k" "K" ]
+                   :l ["l" "L" "2"]
+                   :m ["m" "M" ]
+                   :n ["n" "N"]
+                   :o ["o" "O" ]
+                   :p ["p" "P"]
+                   :q ["q" "Q" "9"]
+                   :r ["r" "R"]
+                   :s ["s" "S"  "5"]
+                   :t ["t" "T"]
+                   :u ["u" "U"]
+                   :v ["v" "V" "7"]
+                   :w ["w" "W"]
+                   :x ["x" "X"]
+                   :y ["y" "Y"]
+                   :z ["z" "Z" "2"]
+                   })
+
+(def x-replacements {
+                   :a ["a" "A" "@" "å" "Å" "∆"]
+                   :b ["b" "B" "ß" "8" "6"]
+                   :c ["c" "C" "ç" ] 
+                   :d ["d" "D"]
+                   :e ["e" "E" "£" "€" "3"]
+                   :f ["f" "F"]
+                   :g ["g" "G" "©"]
+                   :h ["h" "H" ]
+                   :i ["i" "I" "î" "1"]
+                   :j ["j" "J" ]
+                   :k ["k" "K" ]
+                   :l ["l" "L" "¬" "2"]
+                   :m ["m" "M" "µ"]
+                   :n ["n" "N" "~"]
+                   :o ["o" "O" "ø" "Ø" "0"]
+                   :p ["p" "P"]
+                   :q ["q" "Q" "9"]
+                   :r ["r" "R"]
+                   :s ["s" "S"  "5"]
+                   :t ["t" "T"]
+                   :u ["u" "U" "ü" "Ü" "Ω"]
+                   :v ["v" "V" "7"]
+                   :w ["w" "W"]
+                   :x ["x" "X"]
+                   :y ["y" "Y" "¥"]
+                   :z ["z" "Z" "2"]
+                   })
+
 (def dictionary (flatten (into '() (take-csv "resources/public/words.csv"))))
 (def dictionary-size (count dictionary))
 
@@ -42,63 +100,98 @@
         part-3   (nth dictionary (rand-int dictionary-size))
         password  (str part-1 (nth connectors (rand-int  (count connectors))) part-2 )
         ]
-    password
-    )
+    password))
 
-  )
 
-;My home has been invaded by a gaggle of ex-fashion models so I wrote this
 
-(defn get-index "doc-string" []
-
-  ; (debug dictionary-size)
-  (let [
-
-        template  (slurp "resources/public/template.html")
-
-        password (get-pw)
-
-        countr (* (count (.getBytes password)) 1N)
-        per-sec 5000000.0
-        secs-in-year 31536000
-        per-year (* secs-in-year per-sec)
-        y (/ (exp-s countr 24N) per-year  )
-        years (eng y)
-        cheer (nth cheers (rand-int (count cheers)))
-
-        mmm {:password password, :years years, :cheer cheer }
-        ]
-
-    (render template mmm )
-
-    )
-
-  )
+(defn complexify-pw "doc-string" [simple-pw]
+  (def hardz "")
+  (let [simplez (seq simple-pw)]
+    (doseq [c simplez]
+      (if (some #{(str c)} connectors)
+        (def hardz (str hardz (str c)))
+        (let [
+              letter (str c)
+              k (keyword letter)
+              alts (replacements k)
+              newc  (nth alts (rand-int (count alts)))
+              ]
+          (def hardz (str hardz newc)))))
+    hardz))
 
 
 
 
+(defn fiendishly-hardify-pw "doc-string" [simple-pw]
+  (def hardz "")
+  (let [simplez (seq simple-pw)]
+    (doseq [c simplez]
+      (if (some #{(str c)} connectors)
+        (def hardz (str hardz (str c)))
+        (let [
+              letter (str c)
+              k (keyword letter)
+              alts (x-replacements k)
+              newc  (nth alts (rand-int (count alts)))
+              ]
+          (def hardz (str hardz newc)))))
+    hardz))
 
 
 
 
 
-(defroutes app-routes
-  (GET "/" [] (get-index))
+  ;My home has been invaded by a gaggle of ex-fashion models so I wrote this
 
-  (GET "/pw" [] (get-pw))
-  (route/resources "/")
-  (route/not-found "Not Found"))
+  (defn get-index "doc-string" []
 
-(def app
-  (handler/site app-routes))
+    ; (debug dictionary-size)
+    (let [
+
+          template  (slurp "resources/public/template.html")
+
+          password (get-pw)
+
+          complexified (complexify-pw password)
+
+          fiendishly-hard (fiendishly-hardify-pw password)
+
+          countr (* (count (.getBytes password)) 1N)
+          per-sec 5000000.0
+          secs-in-year 31536000
+          per-year (* secs-in-year per-sec)
+          y (/ (exp-s countr 24N) per-year  )
+          years (eng y)
+          cheer (nth cheers (rand-int (count cheers)))
+
+          mmm {:password password, :complexified complexified, :fiendishlyhard fiendishly-hard :years years, :cheer cheer }
+          ]
+      (render template mmm )))
 
 
-;for deployment purposes
-(defn -main
-  [& [port]]
-  (let [port (Integer. (or port
-                           (System/getenv "PORT")
-                           80))]
-    (jetty/run-jetty #'app {:port  port
-                            :join? false})))
+
+
+
+
+
+
+
+  (defroutes app-routes
+    (GET "/" [] (get-index))
+    (GET "/cpw" [] (complexify-pw (get-pw)))
+    (GET "/pw" [] (get-pw))
+    (route/resources "/")
+    (route/not-found "Not Found"))
+
+  (def app
+    (handler/site app-routes))
+
+
+  ;for deployment purposes
+  (defn -main
+    [& [port]]
+    (let [port (Integer. (or port
+                             (System/getenv "PORT")
+                             80))]
+      (jetty/run-jetty #'app {:port  port
+                              :join? false})))
